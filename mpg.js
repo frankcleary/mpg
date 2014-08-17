f0e05fb93ccb6f171a8da8d2158889cb08bc42e9
Frank Cleary <frank@frankcleary.com>
2014-08-16 19:17:34 -0700

    tips added to histogram

--- mpg.js
+++ mpg.js
@@ -3,17 +3,41 @@
 // Read in .csv data. Because d3.csv is asynchronous, all the
 // graph drawing functions are contained within this d3.csv call
 d3.csv("prius_gas.csv", function(error, csvdata) {
-  csvdata.forEach(function(d) {
-    d = +d.MPG;
-  });
+ 
+var binsize = 2;
+var minbin = 36;
+var maxbin = 60;
+var numbins = (maxbin - minbin) / binsize;
+var format = d3.time.format("%m/%d/%Y");
+
+histdata = new Array(numbins);
+for (var i = 0; i < numbins; i++) {
+    histdata[i] = { numfill: 0, meta: "" };
+}
+
+csvdata.forEach(function(d) {
+    var bin = Math.floor((parseFloat(d.MPG) - minbin) / binsize);
+    if (bin.toString() != "NaN") {
+	histdata[bin].numfill += 1;
+	date = format(new Date(d.Date));
+	histdata[bin].meta += "<tr><td>" + d.City + " " + d.State + "</td><td>" + date + "</td><td>" + parseFloat(d.MPG).toFixed(1) + " mpg</td></tr>";
+    }
+
+});
+
+
+//histdata.forEach(function(d) { console.log(+d.numfill) });
+//console.log(histdata);
+//console.log(csvdata);
+//csvdata.forEach(function(d) { console.log(d.City) } );
 
 var margin = {top: 10, right: 30, bottom: 50, left: 60},
     width = 600 - margin.left - margin.right,
     height = 250 - margin.top - margin.bottom;
 
 // Set the limits of the x axis
-var xmin = 35
-var xmax = 60
+var xmin = minbin - 1
+var xmax = maxbin + 1
 
 // This scale is for determining the widths of the histogram bars
 // Must start at 0 or else x(binsize a.k.a dx) will be negative
@@ -51,9 +75,16 @@ var yAxis = d3.svg.axis()
     .orient("left");
 
 var d2 = 3
-
 d3.selectAll(".datainsert")
     .text("test")
+
+var tip = d3.tip()
+  .attr('class', 'd3-tip')
+  .direction('e')
+  .offset([0, 20])
+  .html(function(d) {
+    return '<table id="tiptable">' + d.meta + "</table>";
+  });
 
 // put the graph in the "mpg" div
 var svg = d3.select("#mpg").append("svg")
@@ -62,18 +93,22 @@ var svg = d3.select("#mpg").append("svg")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+svg.call(tip);
+
 // set up the bars
 var bar = svg.selectAll(".bar")
-    .data(data)
+    .data(histdata)
   .enter().append("g")
     .attr("class", "bar")
-    .attr("transform", function(d) { return "translate(" + x2(d.x) + "," + y(d.y) + ")"; });
+    .attr("transform", function(d, i) { return "translate(" + x2(i * binsize + minbin) + "," + y(+d.numfill) + ")"; })
+    .on('mouseover', tip.show)
+    .on('mouseout', tip.hide);
 
 // add rectangles of correct size at correct location
 bar.append("rect")
     .attr("x", 3)
     .attr("width", widthscale * x(data[0].dx) - 3)
-    .attr("height", function(d) { return height - y(d.y); });
+    .attr("height", function(d) { return height - y(+d.numfill); });
 
 // add the x axis and x-label
 svg.append("g")
